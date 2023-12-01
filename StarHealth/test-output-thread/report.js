56534c3b8e3b87bdb8754399af9c45e67d905af2
Farrxhan <148946924+Farrxhan@users.noreply.github.com>
2023-12-01 19:08:00 +0530

    Star Health Project

---
+++ StarHealth/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"328c9fbf-78a0-4751-bd4d-0f7e2f26a697","feature":"Test the starHealth page on Chrome Browser","scenario":"Validate the Star Health Buy Now flow","start":1701437422706,"group":1,"content":"","tags":"","end":1701437472915,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
